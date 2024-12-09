5d3435af1d9553f9ea62ce7b9020ad931928f1bc
Eustaquio <cyrilleustaquio111222@gmail.com>
2024-12-09 21:14:51 +0800

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
-
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import LoginPage from "./routes/Auth/LoginPage"
+import DashboardPage from "./routes/DashboardPage"
 
 const App = () => {
    return (
-      <div>App</div>
+      <div>
+         <BrowserRouter>
+            <Routes>
+               <Route path="/login" element={<LoginPage />} />
+               <Route path="/dashboard" element={<DashboardPage />} />
+            </Routes>
+         </BrowserRouter>
+      </div>
    )
 }
 
