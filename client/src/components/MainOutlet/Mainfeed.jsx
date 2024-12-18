9a0f3b2d2d17c4e34fd5fb15bb30da204f0b33c7
Eustaquio <cyrilleustaquio111222@gmail.com>
2024-12-18 20:26:57 +0800

    update

--- client/src/components/MainOutlet/Mainfeed.jsx
+++ client/src/components/MainOutlet/Mainfeed.jsx
@@ -2,8 +2,10 @@
 
 const Mainfeed = () => {
    return (
-      <div className="max-w-[1200px] mx-auto p-6">
-         Mainfeed
+      <div className="max-w-[1200px] mx-auto p-6 grid grid-cols-[250px_1fr_250px] gap-6">
+         <div className="bg-white">1</div>
+         <div className="bg-white">2</div>
+         <div className="bg-white">3</div>
       </div>
    )
 }
--- client/src/components/MainOutlet/Profile.jsx
+++ client/src/components/MainOutlet/Profile.jsx
@@ -1,9 +1,9 @@
 
 
 const Profile = () => {
-  return (
-    <div className="max-w-[1200px] mx-auto p-6">Profile</div>
-  )
+   return (
+      <div className="max-w-[1200px] mx-auto p-6">Profile</div>
+   )
 }
 
 export default Profile
