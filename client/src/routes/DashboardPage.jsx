1224c21b9a15e11ca461b9c3e0281032107b4bfa
Eustaquio <cyrilleustaquio111222@gmail.com>
2024-12-22 16:28:43 +0800

    connect db

--- client/src/routes/DashboardPage.jsx
+++ client/src/routes/DashboardPage.jsx
@@ -4,7 +4,7 @@ import Navbar from '../components/Navbar';
 const DashboardPage = () => {
    return (
       <div className='h-screen flex flex-col'>
-         <nav className="fixed w-full bg-white border-b border-neutral-300">
+         <nav className="fixed w-full bg-white border-b border-neutral-300 z-10">
             <Navbar />
          </nav>
          <main className='flex-1 mt-[60px] bg-gray-200 overflow-auto'>
--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
 require("dotenv").config();
 const express = require('express');
+const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send('Server is running.');
+   res.send('Server is running.');
 })
 
-const PORT = process.env.PORT;
+const connectWithRetry = () => {
+   mongoose.connect(process.env.MONGODB_URI)
+      .then(() => console.log("MongoDB Connected."))
+      .catch(err => {
+         console.log("MongoDB Connection Error: ", err);
+         console.log("Retrying in 0.5s second...");
+         setTimeout(connectWithRetry, 500);
+      });
+};
+
+connectWithRetry();
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port http://localhost:${PORT}`));
